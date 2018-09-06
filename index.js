9ab23bef63a4ad2fbbeb7b05cf1e4dab9d22a685
gardiner nardini <gardiner.nardini@gmail.com>
2018-09-05 23:17:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = 'd2986644d8d6dfbf9a3989ab99f430dfc0e57a29'
+
+fetch('https://api.github.com/user/repos',{
+    headers:{Authorization:`token ${token}`}
+}).
+    then(res=>res.json()).
+    then(json=>console.log(json))
